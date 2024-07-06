e053a30ef53516ccb39b8ca358c06546a29554ba
mranv <iamanubhavgain@gmail.com>
2024-07-06 13:24:52 +0530

    Added: windows network isolation which is written on rust for now hello world

---
+++ Network Solitude/windows/isolate_windows/target/debug/isolate_windows.d
@@ -0,0 +1,1 @@
+C:\Users\mranv\Desktop\solitude\Network\ Solitude\windows\isolate_windows\target\debug\isolate_windows.exe: C:\Users\mranv\Desktop\solitude\Network\ Solitude\windows\isolate_windows\src\main.rs
