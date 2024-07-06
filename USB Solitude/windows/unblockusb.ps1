81f190f1226e62548b69f6b04152a46d608c8f0f
mranv <iamanubhavgain@gmail.com>
2024-07-06 14:35:35 +0530

    feat: Add administrator check and execution policy change for Windows scripts

--- USB Solitude/windows/unblockusb.ps1
+++ USB Solitude/windows/unblockusb.ps1
@@ -1,3 +1,16 @@
+# Function to check if the script is run as administrator
+function Test-Administrator {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    return $currentUser.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
+if (-not (Test-Administrator)) {
+    Write-Host "This script needs to be run as an administrator. Please run PowerShell as an administrator and try again."
+    exit
+}
+
+# Set the execution policy for the current user
+Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy RemoteSigned -Force
 
 function Unblock-USB {
     $usbStorKey = "HKLM:\SYSTEM\CurrentControlSet\Services\USBSTOR"
